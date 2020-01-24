3719cb8717d9df532fc78f0ae1b8a55abf523813
webcrafters <60077270+webcrafters@users.noreply.github.com>
2020-01-24 22:18:48 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# prototype-ng-basics
-Created with CodeSandbox
---
+++ index.js
@@ -0,0 +1,22 @@
+// const cards = [
+//   {
+//     name: "Somebody",
+//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+//     likes: 2
+//   },
+//   {
+//     name: "Somebody Else",
+//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+//     likes: 3
+//   },
+//   {
+//     name: "Same Person",
+//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+//     likes: 4
+//   },
+//   {
+//     name: "Another Person",
+//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+//     likes: 12
+//   }
+// ];
