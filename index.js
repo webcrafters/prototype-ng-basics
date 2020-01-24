25ce1bbccd38678b95bc5a282ffa242f0cf3f1aa
Dinu Barbu <dinu@DevPlants-MacBook-Pro.local>
2020-01-24 22:23:40 +0200

    completed

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-// const cards = [
-//   {
-//     name: "Somebody",
-//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
-//     likes: 2
-//   },
-//   {
-//     name: "Somebody Else",
-//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
-//     likes: 3
-//   },
-//   {
-//     name: "Same Person",
-//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
-//     likes: 4
-//   },
-//   {
-//     name: "Another Person",
-//     url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
-//     likes: 12
-//   }
-// ];
+const cards = [
+  {
+    name: "Somebody",
+    url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+    likes: 2
+  },
+  {
+    name: "Somebody Else",
+    url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+    likes: 3
+  },
+  {
+    name: "Same Person",
+    url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+    likes: 4
+  },
+  {
+    name: "Another Person",
+    url: "https://innerharbouroptometry.com/wp-content/uploads/2016/05/photo-1438761681033-6461ffad8d80-1030x687.jpe",
+    likes: 5
+  }
+];
+
+const cardElements = document.querySelectorAll('.card');
+
+cardElements.forEach((cardEl, i) => {
+  const cardData = cards[i];
+  cardEl.querySelector('.description').innerHTML = cardData.name;
+  cardEl.querySelector('.image').style.backgroundImage = `url(${cardData.url})`;
+  const likesSpan = cardEl.querySelector('.no_of_likes');
+  likesSpan.innerHTML = cardData.likes;
+  cardEl.querySelector('.likes-star').addEventListener('click', ev => {
+    cardData.likes++;
+    likesSpan.innerHTML = cardData.likes;
+  });
+});
